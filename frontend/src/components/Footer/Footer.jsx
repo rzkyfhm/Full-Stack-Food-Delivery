8a36210897801564151e6bb7b805a231c46b7a8e
rzkyfhm <fhmcode@gmail.com>
2025-08-24 07:57:59 +0700

    first

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel doloribus commodi nemo enim dignissimos? Recusandae sequi tempora molestiae atque illum ab alias nemo, inventore, voluptas fugit minus. Aperiam, quis explicabo est illo laboriosam quas minima necessitatibus.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2025 Tomato.com - All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
